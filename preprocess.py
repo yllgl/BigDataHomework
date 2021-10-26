653bd616d9428d186cac86d08c7f41ff6d70f267
Liang Ganglin <392517304@qq.com>
2021-10-26 22:34:05 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,8 +3,8 @@ xmnlp.set_model('E:/xmnlp-onnx-models-v3/xmnlp-onnx-models')
 with open('test_A.tsv','r',encoding='utf8') as f:
 	for i,line in enumerate(f):
 		a,b = line.strip().split('\t')
-		a = xmnlp.pinyin("".join(spliteKeyWord(a)))
-		b = xmnlp.pinyin("".join(spliteKeyWord(b)))
+		a = xmnlp.pinyin(a)
+		b = xmnlp.pinyin(b)
 		if a==b:
 			with open('test_out.txt','a+',encoding='utf8') as out:
 				out.write(str(i)+"\n")
